97819fe27cb7d3138dc4ff4007a0faaa22d92c93
Dheovani Xavier da Cruz <79609196+Dheovani@users.noreply.github.com>
2021-03-21 22:59:28 -0300

    Update JogoDaForca.java

--- JogoDaForca.java
+++ JogoDaForca.java
@@ -3,51 +3,53 @@ package jogodaforca;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class JogoDaForca {
+public class JogoDaForcaString {
     public static void main(String[] args) {
-        Scanner scannerPalavra = new Scanner(System.in);
-        System.out.println("Digite a palavra chave: ");
-        String palavra = scannerPalavra.nextLine().toLowerCase();
-        char[] forca = palavra.toCharArray();
-        // Acima eu escaneei a palavra e a transformei em um array de char para facilitar o processamento de dados
+        Scanner forcaScanner = new Scanner(System.in);
+        System.out.println("Digite palavra secreta: ");
+        String forca = forcaScanner.nextLine().toLowerCase();
 
-        for(int i = 0; i < 100; i ++) {
+        for(int range = 0; range < 100; range ++){
             System.out.println();
         }
+
+        String[] forcaArray = forca.split("");
+        String[] corretas = new String[forca.length()];
+        String[] incorretas = new String[5];
         int tentativas = 0;
-        char[] corretas = new char[forca.length];
-        char[] incorretas = new char[5];
         boolean status = false;
-        while((tentativas < 5) && (!Arrays.equals(corretas, forca))) {
-            status = false;
-            Scanner scannerLetra = new Scanner(System.in);
+        while(tentativas < 5 && !Arrays.equals(corretas, forcaArray)) {
+            Scanner letraScanner = new Scanner(System.in);
             System.out.println("Escolha uma letra: ");
-            String letraString = scannerLetra.nextLine().toLowerCase();
-            char letraEscolhida = letraString.charAt(0);
-            for(int j = 0; j < forca.length; j ++) {
-                if(letraEscolhida == forca[j]) {
-                    corretas[j] = letraEscolhida;
-                    status = true;
-                    // Se a letra estiver certa, será adicionada ao vetor das corretas e alterará o valor de status
-                    // Caso a última letra digitada antes de uma condição de fim de jogo ser acionada esteja correta, a condição de status será equivalente
+            String letra = letraScanner.nextLine().toLowerCase();
+            if(Arrays.asList(corretas).contains(letra)) {
+                System.out.println("Você já escolheu essa letra. Tente novamente.");
+            } else if (Arrays.asList(incorretas).contains(letra)) {
+                System.out.println("Você já escolheu essa letra. Tente novamente.");
+            } else {
+                if(forca.contains(letra)) {
+                    for(int posicao = 0; posicao < forca.length(); posicao ++) {
+                        if(letra.equals(String.valueOf(forca.charAt(posicao)))) {
+                            corretas[posicao] = letra;
+                            status = true;
+                        }
+                    }
+                } else {
+                    incorretas[tentativas] = letra;
+                    status = false;
+                    tentativas ++;
                 }
+                System.out.print("Letras corretas: ");
+                System.out.println(Arrays.toString(corretas));
+                System.out.print("Letras incorretas: ");
+                System.out.println(Arrays.toString(incorretas));
             }
-            if(!status) {
-                incorretas[tentativas] = letraEscolhida;
-                tentativas ++;
-                // Se a letra estiver errada, será adicionada ao vetor das incorretas e alterará o valor de tentativas
-                // Caso a última letra digitada antes de uma condição de fim de jogo ser acionada esteja incorreta, a condição de status será equivalente
-            }
-            System.out.print("Letras corretas: ");
-            System.out.println(corretas);
-            System.out.print("Letras incorretas: ");
-            System.out.println(incorretas);
         }
 
         if(status) {
-            System.out.println("Parabéns! Você venceu!");
+            System.out.println("Parabéns, você venceu!");
         } else {
-            System.out.println("Que pena! Você perdeu!");
+            System.out.println("Que pena! Você perdeu.");
         }
     }
 }
