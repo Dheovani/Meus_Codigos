cbce148f733fa343b4094fd6686bd5dfe6755c4b
Dheovani Xavier da Cruz <79609196+Dheovani@users.noreply.github.com>
2021-04-12 20:43:47 -0300

    Update Exercicio07.java

--- Cogidos_Java/Codigos_Basicos/Matrizes/Exercicio07.java
+++ Cogidos_Java/Codigos_Basicos/Matrizes/Exercicio07.java
@@ -25,9 +25,19 @@ public class Exercicio07 {
 
         int[][] matrizResult = new int[linhas][colunasB];
         for(int c = 0; c < matrizResult.length; c ++) {
+            // O ponteiro "c" indica em que vetor da matrizResult estamos
             for(int d = 0; d < matrizResult[c].length; d ++) {
+                // O ponteiro "d" indica em que variável do vetor "c" da matrizResult estamos
                 for(int e = 0; e < matrizA[c].length; e ++) {
+                    // O ponteiro "e" indica em que variável do vetor "c" da matrizA estamos
                     matrizResult[c][d] += matrizA[c][e] * matrizB[e][d];
+                    /* Precisamos realizar um cálculo que multiplique os valores das linhas da matrizA
+                     * pelas colunas da matrizB. Sabemos que a coluna da matrizA onde encontra-se o primeiro
+                     * valor utilizado no cálculo será sempre igual à linha da matrizB onde encontra-se o
+                     * segundo valor que será utilizado no cálculo, portanto, basta utilizarmos o ponteiro "c"
+                     * para definir a linha da matrizA onde encontra-se o primeiro valor e o ponteiro "d" para
+                     * definir a coluna da matrizB onde encontra-se o segundo valor utilizado no cálculo.
+                     */
                 }
             }
         }
